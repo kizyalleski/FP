09c55bfb122e7c0be6561eb1cd04ef11ad96bc5d
Maxim <kizyalleski@gmail.com>
2022-07-08 11:28:13 +0300

    data reading is complete

--- North_Valley/src/FP_North_Valley.R
+++ North_Valley/src/FP_North_Valley.R
@@ -1,1 +1,27 @@
-setwd("C:/FP/South_Valley/src")
+setwd("C:/FP/North_Valley/src")
+
+library(tidyverse)
+library(dplyr)
+library(fingerPro)
+library(readxl)
+library(corrr)
+
+# Пример исходных данных
+data <- catchment
+
+# 1) ЧТЕНИЕ ДАННЫХ
+# Северный приток
+
+df_north <-
+  read_excel(path = "../data/North_Valley_data.xlsx") %>% # чтение данных
+  filter(Name != "Размерность") %>% # убираем строку с размерностью
+  select(-X, -Y) %>% # убираем столбцы с координатами
+  rename(id = Name) %>% # переименовываем name в id
+  mutate_at(vars(-Source), ~ as.numeric(.)) %>% # преобразуем в числовой тип
+  na_if(0) %>% # заменяем нули на na
+  as.data.frame() # преобразуем к типу дата фрейм из тибла
+  
+
+# преобразование размерности
+
+
