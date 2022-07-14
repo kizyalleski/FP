0b1729ff9ebd5a08e846ae9a5a4351f8d17dede7
Maxim <kizyalleski@gmail.com>
2022-07-14 20:11:22 +0300

    morphology FP for whole catchment mix-2024

--- Whole_Catchment/src/morphology/wlole_catchment_morphology.R
+++ Whole_Catchment/src/morphology/wlole_catchment_morphology.R
@@ -1,4 +1,4 @@
-setwd("C:/FP/Whole_Catchment/src")
+setwd("C:/FP/Whole_Catchment/src/morphology")
 
 library(tidyverse)
 library(dplyr)
@@ -12,7 +12,7 @@ data <- catchment
 # 1) ЧТЕНИЕ и подготовка ДАННЫХ
 
 df_whole <-
-  read_excel(path = "../data/morph_data_3s_mix-2025.xlsx") %>% # чтение данных
+  read_excel(path = "../../data/morph_data_3s_mix-2024.xlsx") %>% # чтение данных
   filter(Name != "Размерность") %>% # убираем строку с размерностью
   select(-X, -Y) %>% # убираем столбцы с координатами
   rename(id = Name) %>% # переименовываем name в id
@@ -26,7 +26,7 @@ mix_na <-
   filter(Source == "Mix") %>% # оставляем только строку целевого образца
   select_if(is.na) %>% # выбираем столбцы с na
   gather(var, val) %>% # переменную и значение ориентируем вертикально
-  pull(var) # извлекаем вектор названий
+    pull(var) # извлекаем вектор названий
 
 # получение итогового набора данных
 df <-
@@ -60,27 +60,21 @@ df_lda <-
   filter(id != 2005) %>%
   filter(id != 2015) %>%
   filter(id != 27) %>%
-  filter(id != 4004) %>% 
-  filter(id != 20) %>%
+  filter(id != 2021) %>%
+  filter(id != 2019) %>%
+  filter(id != 15) %>%
+  filter(id != 20) %>% 
   filter(id != 28) %>% 
-  filter(id != 2007) %>%
-  filter(id != 2011) %>%
-  filter(id != 2012) %>%
-  filter(id != 2013) %>% 
-  filter(id != 13) %>%
-  filter(id != 2019) %>% 
-  filter(id != 2021) %>% 
   filter(id != 2020) %>%
-  filter(id != 24) %>% 
-  filter(id != 9) %>% 
-  filter(id != 3003) %>%
-  filter(id != 3004) %>% 
-  filter(id != 6) %>% 
-  filter(id != 14) %>%
+  filter(id != 2003) %>%
+  filter(id != 2029) %>%
+  filter(id != 6) %>%
   filter(id != 17) %>% 
-  filter(id != 25) %>% 
-  filter(id != 22) %>% 
-  filter(id != 2018)
+  filter(id != 3003) %>% 
+  filter(id != 3004) %>% 
+  filter(id != 21) %>% 
+  filter(id != 24)
+  
 
 df_lda %>% 
   LDAPlot(text = T)
@@ -94,7 +88,7 @@ DFATest(df_lda, niveau = 0.05)
 
 # 5) БОКСПЛОТЫ
 df %>% 
-  select(id, Source, Al, Zn, sluda, smektit, kps, plagioklaz) %>% 
+  select(id, Source, Al, Mn, Zn, smektit, kps, plagioklaz) %>% 
   gather(elem, cons, -id, -Source) %>% 
   ggplot(aes(x = Source,
              y = cons,
@@ -106,13 +100,13 @@ df %>%
 
 # 6) ПОДТВЕРЖДЕНИЕ ТРАССЕРОВ
 df_lda %>% 
-  select(id, Source, Al,Zn, sluda, smektit, kps, plagioklaz) %>% 
+  select(id, Source, Al, Mn, Zn, smektit, kps, plagioklaz) %>% 
   LDAPlot(text = T)
 
 # 7) Размешивание
 results <- 
   df_lda %>% 
-  select(id, Source, Al, Zn, sluda, smektit, kps, plagioklaz) %>% 
+  select(id, Source, Al, Mn, Zn, smektit, kps, plagioklaz) %>% 
   unmix(samples = 200, iter = 300)
 
 results %>% 
